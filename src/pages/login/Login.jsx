911e7652e4a475bc202f59b252248a1f7b617a72
W H Mahesh Madushnaka <136902782+MaheshMadushanka@users.noreply.github.com>
2026-02-07 23:49:37 +0530

    Implement navigation after successful login
    Added navigation to dashboard after login submission.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Mail, Lock, Eye, EyeOff } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 import Logo from "../../assets/vtclogo.jpeg";
 
 function Login() {
+  const nav = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: '',
@@ -18,8 +20,10 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
     // Handle login logic here
     console.log('Login submitted:', formData);
+     nav('/dashboard');
   };
 
   return (
